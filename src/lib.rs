8961c4bfeb8c9e6601e712043c9e76f788f9f049
DLHTX <949729789@qq.com>
2023-05-22 12:23:24 +0800

    add rust sdk

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,7 @@
-use sdk::{log_info, get_data_as_str};
-use serde_json::Value;
-
-mod sdk;
-mod types;
+use ws_sdk::log::log_info;
 
 #[no_mangle]
-pub extern "C" fn alloc(size: i32) -> *mut u8 {
-    let mut buf: Vec<u8> = Vec::with_capacity(size as _);
-    let ptr = buf.as_mut_ptr();
-    std::mem::forget(buf);
-    return ptr;
-}
-
-#[no_mangle]
-// This handler will be matched by the default Project event strategy in W3bstream
-pub extern "C" fn start(event_id: i32) -> i32 {
-    log_info("start from rust");
-
-    let data_u8 = match sdk::get_data(event_id) {
-        Some(data) => data,
-        _ => {
-            sdk::log_error("failed to get data from event");
-            return -1;
-        }
-    };
-    
-    let data_str = match String::from_utf8(data_u8) {
-        Ok(data) => data,
-        _ => {
-            sdk::log_error("failed to convert data to string");
-            return -1;
-        }
-    };
-
-    sdk::log_info(&format!("data: {}", data_str));
-    sdk::log_info(&format!("data: {}", data_str));
+pub extern "C" fn start(_: i32) -> i32 {
+    log_info("hello world!");
     return 0;
 }
